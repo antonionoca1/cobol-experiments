bcaeea4f1c0d2dfe9a4bfa4fc5bfc23f0ac8a5ee
Antonio <antonionoca@gmail.com>
2025-07-14 10:37:33 +0100

    [cbl-1] hello world in cobol on windows

---
+++ hello-world/hello.cbl
@@ -0,0 +1,13 @@
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. HELLO-WORLD.
+      * This is a simple COBOL "Hello World" program.
+      * It demonstrates the basic structure of a COBOL program.
+
+       PROCEDURE DIVISION.
+       MAIN-LOGIC SECTION.
+           DISPLAY "Hello, World!".
+      * The DISPLAY statement writes output to the console.
+      * "Hello, World!" is the literal string to be displayed.
+
+           STOP RUN.
+      * STOP RUN terminates the execution of the program.
